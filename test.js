3486af64aabb9c7c373f8136db4e0f673a087ed0
Aswathy <aswathyk2805@gmail.com>
2023-05-21 23:09:12 +0530

    Robotics Project

---
+++ test.js
@@ -0,0 +1,66 @@
+<html>
+    <head/>
+        <body/>
+            <form/>
+                <h1 id="title" Fill the following registration form
+                h1>
+
+
+                <label for="N">Name: </label>
+                <input type="text" id = "name" placeholder="Name" required/>
+                <br/>
+                <br/>
+                <label for ="RN">Registeration Number: </label>
+                <input type="text" placeholder="Registeration Number" required/>
+                <br/>
+                <br/>
+                <label for ="G">Gender: </label>
+                <label for ="F">Female: </label>
+                <input type="radio" required/>
+                <label for ="M">Male: </label>
+                <input type="radio" required/>
+                <label for ="T">Transgender: </label>
+                <input type="radio" required/>
+                <br/>
+                <br/>
+                <label for ="DOB">Date of Birth: </label>
+                <input type="date" required/>
+                <br/>
+                <br/>
+                <label for ="E">E-Mail Address: </label>
+                <input type="Email" placehoolde="E-Mail Address" required/>
+                <br/>
+                <br/>
+                <label for="Number">Whatsapp Number: </label>
+                <input type="Password" placeholder="Whatsapp Number" required/>
+                <br/>
+                <br/>
+                <label from "q1"> Which domain you are interested in <br>
+                    <textarea name="ans1" id="" cols="40" rows="5"></textarea><required/>
+                </label>
+                <br/>
+                <br/>
+                <label from "q2"> Why did you choose this domain <br>
+                    <textarea name="ans1" id="" cols="40" rows="5"></textarea><required/> 
+                </label>
+                <br/>
+                <br/>
+                </label>
+                <h3>Which all applications you have used before? </h3><required/>
+                <input type = "checkbox" name ="chkSoftwareSkills"> Fusion 360
+                <input type = "checkbox" name ="chkSoftwareSkills"> Python
+                <input type = "checkbox" name ="chkSoftwareSkills"> Java
+                <input type = "checkbox" name ="chkSoftwareSkills"> C++
+                <input type = "checkbox" name ="chkSoftwareSkills"> C 
+                <br/>
+                <label from "q3"> </input></input></input> Other Programming Platform (if any)
+                    knowledge <br/>
+                    <textarea name="ans3" id="" cols="40" rows="5"></textarea>
+                <label/>
+                <br/>
+                <br/>
+                <button type="submit">SUBMIT</button> 
+            <form/>
+        <body/>
+    <head/>
+<html/>
